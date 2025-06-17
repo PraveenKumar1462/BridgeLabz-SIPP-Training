e4c4819268587007a905b44d1ba593be2b4a93f6
Praveen Kumar <praveenkumar19807@gmail.com>
2025-06-17 18:10:24 +0530

    [Praveen Kumar] Add SpringSeason

---
+++ Day2_ControlFlow/SpringSeason.java
@@ -0,0 +1,29 @@
+package Day2_ControlFlow;
+
+import java.util.Scanner;
+
+public class SpringSeason {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter month (1-12): ");
+        int month = input.nextInt();
+        System.out.print("Enter day: ");
+        int day = input.nextInt();
+
+        boolean isSpring = false;
+        if (month == 3 && day >= 20) {
+            isSpring = true;
+        } else if (month > 3 && month < 6) {
+            isSpring = true;
+        } else if (month == 6 && day <= 20) {
+            isSpring = true;
+        }
+
+        if (isSpring) {
+            System.out.println("It's a Spring Season");
+        } else {
+            System.out.println("Not a Spring Season");
+        }
+        input.close();
+    }
+}
