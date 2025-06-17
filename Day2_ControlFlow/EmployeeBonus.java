9904e04ac7113d75578189e5a58e996a858db55f
Praveen Kumar <praveenkumar19807@gmail.com>
2025-06-17 18:38:18 +0530

    [Praveen Kumar] Add EmployeeBonus

---
+++ Day2_ControlFlow/EmployeeBonus.java
@@ -0,0 +1,21 @@
+package Day2_ControlFlow;
+
+import java.util.Scanner;
+
+public class EmployeeBonus {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter salary: ");
+        double salary = input.nextDouble();
+        System.out.print("Enter years of service: ");
+        int years = input.nextInt();
+
+        if (years > 5) {
+            double bonus = salary * 0.05;
+            System.out.println("Bonus amount: " + bonus);
+        } else {
+            System.out.println("No bonus");
+        }
+        input.close();
+    }
+}
